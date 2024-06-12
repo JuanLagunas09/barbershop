c8356180ac0ee254a330088549eb58e599b4be9f
Juan J. Lagunas <lagunasjuan_isc09@hotmail.com>
2024-06-12 17:05:31 -0600

    add hero section and refactor header transparen

--- src/scripts/MenuFunction.ts
+++ src/scripts/MenuFunction.ts
@@ -1,15 +1,48 @@
 const btnNavbarToggle = document.getElementById("btn-navbar-toggle");
 const navMenu = document.getElementById("navbar-menu");
+const headerNav = document.getElementById("header-nav");
 
 function closeMenu() {
+  transparentBar();
   btnNavbarToggle?.setAttribute("aria-expanded", "false");
   navMenu?.classList.add("hidden");
 }
+
+function transparentBar() {
+  headerNav?.classList.remove("shadow-sm");
+  headerNav?.classList.add("bg-transparent");
+  headerNav?.classList.remove("bg-primary-color");
+}
+
+function solidBar() {
+  headerNav?.classList.add("shadow-sm");
+  headerNav?.classList.remove("bg-transparent");
+  headerNav?.classList.add("bg-primary-color");
+}
+
+function changeBar() {
+  if (window.scrollY > 0) {
+    solidBar();
+  } else {
+    transparentBar();
+  }
+}
+
+window.addEventListener("scroll", () => {
+  changeBar();
+});
+
+
 
 // Toggle the menu
 btnNavbarToggle?.addEventListener("click", () => {
   btnNavbarToggle?.setAttribute("aria-expanded", "true");
   navMenu?.classList.toggle("hidden");
+  if (navMenu?.classList.contains("hidden")) {
+    changeBar();
+  } else {
+    solidBar();
+  }
 });
 
 // Close the menu when clicking outside
@@ -34,4 +67,8 @@ document.querySelectorAll("#navbar-menu a").forEach((link) => {
   link.addEventListener("click", () => {
     closeMenu();
   });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  changeBar();
 });
